58abbbde0a3e202bb603e855c4ae0c966a30d8f2
Danila Klimenko <danila.klimenko@me.com>
2013-11-21 00:13:53 +0100

    Updated the client side for the new communication logic.

--- src/client/KVStore.java
+++ src/client/KVStore.java
@@ -8,166 +8,186 @@ import java.net.Socket;
 import CustomExceptions.ServerConnectionException;
 
 import common.messages.KVMessage;
+import common.messages.KVMessageRaw;
+import common.messages.NetworkMessage;
+import java.text.ParseException;
 
 public class KVStore implements KVCommInterface {
 
-        private Socket objSocketClient;
-        private InputStream objSocketInputStream;
-        private OutputStream objSocketOutPutStream;
-        private final String strServerAdress;
-        private final int nServerPort;
+    private Socket objSocketClient;
+    private InputStream objSocketInputStream;
+    private OutputStream objSocketOutPutStream;
+    private final String strServerAdress;
+    private final int nServerPort;
+
+    /**
+     * Get the IP address of server.
+     *
+     * @return the IP address of server to whom socket is connected.
+     */
+    public String GetServerIP() {
+        return strServerAdress;
+    }
+
+    /**
+     * Get the port number of server.
+     *
+     * @return port number to whom the socket is connected.
+     */
+    public int GetServerPort() {
+        return nServerPort;
+    }
+
+    public KVStore() {
+        strServerAdress = "";
+        nServerPort = -1;
+        objSocketClient = null;
+        objSocketInputStream = null;
+        objSocketOutPutStream = null;
+    }
+
+    /**
+     * Initialize KVStore with address and port of KVServer
+     *
+     * @param address the address of the KVServer
+     * @param port the port of the KVServer
+     */
+    public KVStore(String address, int port) {
+        strServerAdress = address;
+        nServerPort = port;
+    }
+
+    @Override
+    public void connect() throws Exception {
+
+        if (objSocketClient == null && !strServerAdress.isEmpty() && nServerPort != -1) {
+            objSocketClient = new Socket(strServerAdress, nServerPort);
+            objSocketInputStream = objSocketClient.getInputStream();
+            objSocketOutPutStream = objSocketClient.getOutputStream();
+            RecieveMessage();
+        }
+    }
+
+    @Override
+    public void disconnect() {
+        try {
+            if (objSocketInputStream != null) {
+                objSocketInputStream.close();
+            }
+            if (objSocketOutPutStream != null) {
+                objSocketOutPutStream.close();
+            }
+            if (objSocketClient != null) {
+                objSocketClient.close();
+            }
+        } catch (IOException ex) {
+        }
+
+        objSocketInputStream = null;
+        objSocketOutPutStream = null;
+        objSocketClient = null;
+    }
+
+    @Override
+    public KVMessage put(String key, String value) throws Exception {
+        KVMessage kvmsg = new KVMessageRaw(KVMessage.StatusType.PUT, key, value);
         
-        /**
-        * Get the IP address of server.
-        * @return the IP address of server to whom socket is connected.
-        */
-        public String GetServerIP() 
-        { 
-            return strServerAdress;
-        }
+        return this.kvRequest(kvmsg);
+    }
+
+    @Override
+    public KVMessage get(String key) throws Exception {
+        KVMessage   kvmsg = new KVMessageRaw(KVMessage.StatusType.GET, key, null);
+        
+        return this.kvRequest(kvmsg);
+    }
     
-        /**
-         * Get the port number of server.
-         * @return port number to whom the socket is connected.
-         */
-        public int GetServerPort()
-        { 
-            return nServerPort; 
+    public KVMessage kvRequest(KVMessage kv_out) throws IOException {
+        NetworkMessage netmsg = new NetworkMessage(KVMessageRaw.marshal(kv_out));
+        netmsg.writeTo(objSocketOutPutStream);
+        
+        KVMessage kv_in;
+        netmsg = NetworkMessage.readFrom(objSocketInputStream);
+        try {
+            kv_in = KVMessageRaw.unmarshal(netmsg.getData());
+            // TODO: remove the next line!
+            System.out.println(kv_in.getStatus() + " " + kv_in.getKey() + " " + kv_in.getValue());
+        } catch (ParseException e) {
+            kv_in = null;
+            String error_message = new String(netmsg.getData());
+            // TODO: remove the next line!
+            System.out.println(error_message);
+            throw new IOException(error_message);
         }
         
-        public KVStore()
-        {
-            strServerAdress = "";
-            nServerPort = -1;
-            objSocketClient = null;
-            objSocketInputStream = null;
-            objSocketOutPutStream = null;
+        return kv_in;
+    }
+
+    /**
+     * Send the message to the server to whom socket is connected.
+     *
+     * @param strMessage Message which has to be send to server.
+     * @throws IOException
+     * @throws ServerConnectionException
+     */
+    private void SendMessage(String strMessage) throws IOException, ServerConnectionException {
+        if (objSocketOutPutStream != null) {
+            byte[] msgBytes = strMessage.getBytes();
+            objSocketOutPutStream.write(msgBytes);
+            objSocketOutPutStream.write(0x0D);
+            objSocketOutPutStream.flush();
+        } else {
+            throw new ServerConnectionException("Error! Not connected!");
         }
-	/**
-	 * Initialize KVStore with address and port of KVServer
-	 * @param address the address of the KVServer
-	 * @param port the port of the KVServer
-	 */
-	public KVStore(String address, int port) {
-            strServerAdress = address;
-            nServerPort = port;
-	}
-	
-	@Override
-	public void connect() throws Exception {
-            
-            if (objSocketClient == null && !strServerAdress.isEmpty() && nServerPort != -1)
-            {
-                objSocketClient = new Socket(strServerAdress, nServerPort);
-                objSocketInputStream = objSocketClient.getInputStream();
-                objSocketOutPutStream = objSocketClient.getOutputStream();
-                RecieveMessage();
-            }
-	}
+    }
 
-	@Override
-	public void disconnect(){
-            try
-            {
-                if (objSocketInputStream != null)
-                    objSocketInputStream.close();
-                if (objSocketOutPutStream != null)
-                    objSocketOutPutStream.close();
-                if (objSocketClient != null)
-                    objSocketClient.close();
-            }
-            catch(IOException ex)
-            {
+    /**
+     * Receive the message from the server to whom socket is connected.
+     *
+     * @return Read message from the server.
+     * @throws IOException
+     * @throws ServerConnectionException
+     */
+    private String RecieveMessage() throws IOException, ServerConnectionException {
+        String strRecvMsg = new String();
+
+        if (objSocketInputStream != null) {
+            byte[] recieveMsgBytes;
+            byte[] readBuf1 = new byte[1];
+            int readBytes = objSocketInputStream.read(readBuf1);
+            if (objSocketInputStream.available() > 0) {
+                byte[] readBuf2 = new byte[objSocketInputStream.available()];
+                readBytes = objSocketInputStream.read(readBuf2);
+                recieveMsgBytes = new byte[readBuf2.length + readBuf1.length];
+                System.arraycopy(readBuf1, 0, recieveMsgBytes, 0, 1);
+                System.arraycopy(readBuf2, 0, recieveMsgBytes, 1, readBuf2.length);
+            } else {
+                recieveMsgBytes = new byte[1];
+                recieveMsgBytes[0] = readBuf1[0];
             }
 
-            objSocketInputStream = null;
-            objSocketOutPutStream = null;
-            objSocketClient = null;
-	}
-
-	@Override
-	public KVMessage put(String key, String value) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public KVMessage get(String key) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-        /**
-        * Send the message to the server to whom socket is connected.
-        * @param strMessage Message which has to be send to server.
-        * @throws IOException
-        * @throws ServerConnectionException 
-        */
-       private void SendMessage(String strMessage) throws IOException, ServerConnectionException
-       {
-           if (objSocketOutPutStream != null)
-           {
-               byte[] msgBytes = strMessage.getBytes();
-               objSocketOutPutStream.write(msgBytes);
-               objSocketOutPutStream.write(0x0D);
-               objSocketOutPutStream.flush();
-           }
-           else 
-               throw new ServerConnectionException("Error! Not connected!");
-       }
-    
-        /**
-         * Receive the message from the server to whom socket is connected.
-         * @return Read message from the server.
-         * @throws IOException
-         * @throws ServerConnectionException 
-         */
-        private String RecieveMessage() throws IOException, ServerConnectionException
-        {
-            String strRecvMsg = new String();
-
-            if (objSocketInputStream != null)
-            {
-                byte []recieveMsgBytes;
-                byte[] readBuf1 = new byte[1];
-                int readBytes = objSocketInputStream.read(readBuf1);
-                if (objSocketInputStream.available() > 0)
-                {
-                    byte[] readBuf2 = new byte[objSocketInputStream.available()];
-                    readBytes = objSocketInputStream.read(readBuf2);
-                    recieveMsgBytes = new byte[readBuf2.length + readBuf1.length];
-                    System.arraycopy(readBuf1, 0, recieveMsgBytes, 0, 1);
-                    System.arraycopy(readBuf2, 0, recieveMsgBytes, 1, readBuf2.length);
-                }
-                else
-                {
-                    recieveMsgBytes = new byte[1];
-                    recieveMsgBytes[0] = readBuf1[0];
-                }
-
-                if (readBytes != 0 && readBytes != -1)
-                {
-                    strRecvMsg = new String(recieveMsgBytes, 0, recieveMsgBytes.length-2);
-                }
+            if (readBytes != 0 && readBytes != -1) {
+                strRecvMsg = new String(recieveMsgBytes, 0, recieveMsgBytes.length - 2);
             }
-            else 
-                throw new ServerConnectionException("Error! Not connected!");
-
-            return strRecvMsg;
+        } else {
+            throw new ServerConnectionException("Error! Not connected!");
         }
-    
-        /**
-         * Send and receive the message to/from the server respectively.
-         * @param strMessage Message which has to be send to server.
-         * @return Read message from the server.
-         * @throws IOException
-         * @throws ServerConnectionException 
-         */
-        @Override
-        public String SendRecvMessage(String strMessage) throws IOException, ServerConnectionException
-        {
-            SendMessage(strMessage);
-            return RecieveMessage();
-        }
-	
+
+        return strRecvMsg;
+    }
+
+    /**
+     * Send and receive the message to/from the server respectively.
+     *
+     * @param strMessage Message which has to be send to server.
+     * @return Read message from the server.
+     * @throws IOException
+     * @throws ServerConnectionException
+     */
+    @Override
+    public String SendRecvMessage(String strMessage) throws IOException, ServerConnectionException {
+        SendMessage(strMessage);
+        return RecieveMessage();
+    }
+
 }
